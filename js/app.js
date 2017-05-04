efd46b682c5a45f1c534699181305c9024ee8c4d
Sven Molhuijsen <s.molhuijsen@gmail.com>
2017-05-05 00:08:42 +0200

    added comments to modal functions

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 $(document).foundation();
 
-
 //Modal afsluiten
 $(".signin-register-modal").click(function () {
     $('.signin-register-modal').fadeOut(300);
@@ -8,29 +7,36 @@ $(".signin-register-modal").click(function () {
     return false;
 });
 
+//inlog-pop modal openen
 $('.login_button, .signin').on('click', function () {
     showModal();
     showSignIn();
 });
+
+//registreer modal openen
 $('.signup_button, .register').on('click', function () {
 
     showModal();
     showRegister();
 });
+//reset modal openen
 $('.reset').on('click', function () {
     showReset();
 });
 
+//achtergrond en venster openen
 function showModal() {
     $('.signin-register-modal').fadeIn(300);
 }
+
+//het openen van inlog scherm binnen het modal
 function showSignIn() {
     $('#register, #reset').hide();
     $('#login').show();
     $('.switcher').children().removeClass("active");
     $('.switcher .signin').addClass("active");
 }
-
+//het openen van registreren scherm binnen het modal
 function showRegister() {
     $('#login, #reset').hide();
     $('#register').show();
@@ -38,6 +44,7 @@ function showRegister() {
     $('.switcher .register').addClass("active");
 }
 
+//het openen van reset scherm binnen het modal
 function showReset() {
     $('#login, #register').hide();
     $('.switcher').children().removeClass("active");
