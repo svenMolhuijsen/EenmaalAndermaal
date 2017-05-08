3a00d15f82c5245e150d163597a9ffb3b4dcbdaf
luukdewildt <dewildtluuk@gmail.com>
2017-05-08 15:00:32 +0200

    Categoriepagina Front end

---
+++ categoriepagina.php
@@ -0,0 +1,76 @@
+<?php
+
+include("php/layout/header.php");
+
+?>
+
+<hr>
+    <div class="large-8 row categoriepagina">
+        <div class="side-nav-block large-3 columns">
+            <ul class="side-nav">
+                <li><a href="#">Populaire categoriën</a></li>
+                <li><a href="#">Computers</a></li>
+                <li><a href="#">Auto's</a></li>
+                <li><a href="#">Kleding</a></li>
+                <li><a href="#">Sport</a></li>
+                <li><a href="#">Tuin & Terras</a></li>
+                <li><a href="#">Watersport & boten</a></li>
+                <li><a href="#">Boeken</a></li>
+                <li><a href="#">Ticket & kaartjes</a></li>
+                <li><a href="#">Dieren & toebehoren</a></li>
+                <li><a href="#">Fietsen & brommers</a></li>
+                <li><a href="#">Huis & inrichting</a></li>
+            </ul></div>
+
+        <div class="large-9 columns categorien">
+            <div class="row small-up-2 large-up-5">
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/300x300">
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/600x600">
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/600x600">
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/600x600">
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/600x600">
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/600x600">
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/600x600">
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/600x600">
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/600x600">
+                </div>
+                <div class="column">
+                    <img class="thumbnail" src="http://placehold.it/600x600">
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <hr>
+<div class="row">
+    <div class="clearfix">
+        <h4 class="float-left">Populaire producten</h4>
+        <!--<a href="#" class="button hollow float-right">view more ></a>!-->
+    </div>
+    <div class="small-up-2 medium-up-3 large-up-6 columns-12 clearfix">
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
+    </div>
+</div>
