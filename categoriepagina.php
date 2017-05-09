4ea1da28ae470e69bae695a13d9223f02d965757
luukdewildt <dewildtluuk@gmail.com>
2017-05-09 20:32:10 +0200

    Responsive categorie pagina + css bijgewerkt

--- categoriepagina.php
+++ categoriepagina.php
@@ -6,26 +6,98 @@ include("php/layout/header.php");
 
 <hr>
     <div class="large-8 row categoriepagina">
-        <div class="side-nav-block large-3 columns">
-            <ul class="side-nav">
-                <li><a href="#">Populaire categoriën</a></li>
-                <li><a href="#">Computers</a></li>
-                <li><a href="#">Auto's</a></li>
-                <li><a href="#">Kleding</a></li>
-                <li><a href="#">Sport</a></li>
-                <li><a href="#">Tuin & Terras</a></li>
-                <li><a href="#">Watersport & boten</a></li>
-                <li><a href="#">Boeken</a></li>
-                <li><a href="#">Ticket & kaartjes</a></li>
-                <li><a href="#">Dieren & toebehoren</a></li>
-                <li><a href="#">Fietsen & brommers</a></li>
-                <li><a href="#">Huis & inrichting</a></li>
-            </ul></div>
+        <div class="side-nav-block medium-3 large-3 columns">
 
-        <div class="large-9 columns categorien">
-            <div class="row small-up-2 large-up-5">
+            <ul class="side-nav accordion" data-accordion>
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Populaire categorien</a>
+                    <div class="accordion-content show-for-small-only" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Computers</a>
+                    <div class="accordion-content show-for-small-only" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Auto's</a>
+                    <div class="accordion-content show-for-small-only" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Kleding</a>
+                    <div class="accordion-content show-for-small-only" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Sport</a>
+                    <div class="accordion-content show-for-small-only" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Tuin & Terras</a>
+                    <div class="accordion-content show-for-small-only" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Watersport & boten</a>
+                    <div class="accordion-content show-for-small-only" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Boeken</a>
+                    <div class="accordion-content show-for-small-only" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Ticket & kaartjes</a>
+                    <div class="accordion-content show-for-small-only" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Dieren & toebehoren</a>
+                    <div class="accordion-content show-for-small-only" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Fietsen & brommers</a>
+                    <div class="accordion-content" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+
+                <li class="accordion-item" data-accordion-item>
+                    <a href="#" class="accordion-title">Huis & inrichting</a>
+                    <div class="accordion-content" data-tab-content>
+                        <a href="#">Sub categorie 1</a>
+                    </div
+                </li>
+            </ul>
+        </div>
+
+        <div class="medium-9 large-9 columns categorien">
+            <div class="row small-up-2 medium-up-4 large-up-5 show-for-medium">
                 <div class="column">
-                    <img class="thumbnail" src="http://placehold.it/300x300">
+                    <img class="thumbnail" src="http://placehold.it/600x600">
                 </div>
                 <div class="column">
                     <img class="thumbnail" src="http://placehold.it/600x600">
@@ -66,12 +138,12 @@ include("php/layout/header.php");
         <!--<a href="#" class="button hollow float-right">view more ></a>!-->
     </div>
     <div class="small-up-2 medium-up-3 large-up-6 columns-12 clearfix">
-        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
-        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
-        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
-        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
-        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
-        <div class="column column-block"><img src="http://placehold.it/150x300" alt="">Product naam<br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt=""><h3>Product naam</h3><br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt=""><h3>Product naam</h3><br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt=""><h3>Product naam</h3><br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt=""><h3>Product naam</h3><br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt=""><h3>Product naam</h3><br>Test</div>
+        <div class="column column-block"><img src="http://placehold.it/150x300" alt=""><h3>Product naam</h3><br>Test</div>
     </div>
 </div>
 
