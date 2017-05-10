61483c8114f3fea55a409fd6edb6662a5ae9c702
Stein Carsten Sinke <sinke.carsten95@icloud.com>
2017-05-10 17:45:11 +0200

    completed test criteria

--- veilingpagina.php
+++ veilingpagina.php
@@ -30,6 +30,7 @@ include("php/layout/breadcrumbs.php");
 <div class="row">
     <div class="large-6 columns">
             <h1><strong><?php echo($veiling->getTitel()); ?></strong></h1>
+            <h5><?php echo($veiling->getCategorieId());?></h5>
 
             <h4 class="titel"><strong>Verkoper:</strong></h4>
             <h5><?php echo($gebruiker->getVoornaam()." ".$gebruiker->getAchternaam()); ?></h5>
@@ -63,6 +64,12 @@ include("php/layout/breadcrumbs.php");
             ?>
         </div>
         <div class="row">
+            <p>Datums</p>
+            <span>Begin Datum: <?php echo($veiling->getBeginDatum());?></span><br><span>Eind Datum: <?php echo($veiling->getEindDatum());?></span>
+            <h4><strong>Einde veiling</strong></h4>
+            <span id="timer"></span>
+        </div>
+        <div class="row">
             <div class="large-6 columns">
                 <h4><strong>Verkoop Prijs:</strong></h4>
                 <h5><?php echo("€".round($veiling->getVerkoopPrijs(), 2)); ?></h5>
@@ -71,10 +78,43 @@ include("php/layout/breadcrumbs.php");
             <div class="large-6 columns">
                 <h4><strong>Hoogste bod:</strong></h4>
                 <h5><?php echo("€".round($veiling->getStartPrijs())); ?></h5>
+                <input type="text" placeholder="bedrag">
                 <a href="#" class="button" style="width: 100%; margin: 5% 0;">Bieden</a>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+var eindDatum = "<?php echo($veiling->getEindDatum())?>"
+// Set the date we're counting down to
+var countDownDate = new Date(eindDatum).getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+    // Get todays date and time
+    var now = new Date().getTime();
+    
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    // Output the result in an element with id="demo"
+    document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+    
+    // If the count down is over, write some text 
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}, 1000);
+</script>
 
 <?php include("php/layout/footer.php") ?>
