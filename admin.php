40017bfceec4fb9f0dc42a1df94c5906c84ab778
Stein Carsten Sinke <sinke.carsten95@icloud.com>
2017-05-12 14:56:23 +0200

    added overzicht and add payment info

--- admin.php
+++ admin.php
@@ -6,22 +6,38 @@ include("php/layout/breadcrumbs.php");
 
 <main class="row">
     <ul class="tabs" id="admintabs" data-tabs>
-        <li class="tabs-title"><a href="#beheer">beheer</a></li>
+        <li class="tabs-title is-active"><a href="#overzicht">overzicht</a></li>
         <li class="tabs-title"><a href="#betalingen">betaalgegevens</a></li>
         <li class="tabs-title"><a href="#categorie">Categorie toevoegen</a></li>
     </ul>
 
     <div class="tabs-content" data-tabs-content="admintabs">
-        <div class="tabs-panel" id="beheer">
-            <p>Test123</p>
+        <div class="tabs-panel" id="overzicht">
+            <div class="row expanded">
+                <h4><strong>Verkoop gegevens</strong></h4>
+                <hr>
+                <h5><strong>Vestiging</strong></h5>
+                <p>Technovium<br>vergertlaan 10<br>9421BS<br>Nijmegen</p>
+            </div>
+            <div class="row expanded">
+                <h4><strong>Betaal gegevens</strong></h4>
+                <hr>
+                <h5>Creditcard</h5>
+                <p>NL 35 INGB 0004 5712 32</p>
+            </div>
         </div>
         <div class="tabs-panel" id="betalingen">
-            <p>Test123</p>
+            <select name="" id="">
+                <option value="">Paypal</option>
+                <option value="">Creditcard</option>
+                <option value="">Vooruitbetaling</option>
+            </select>
+            <input type="text" placeholder="cardnumber">
+            <div class="button" type="submit" value="Submit">Submit</div>
         </div>
         <div class="tabs-panel" id="categorie">
-            <div class="row expanded" style="padding-top:4em;">
+            <div class="row expanded" style="margin-top:0;">
                 <input type="text" placeholder="new Categorie">
-            
                 <ul class="vertical menu" data-accordion-menu data-multi-expand="false">
                     <li>
                         <a href="#">Computers</a>
