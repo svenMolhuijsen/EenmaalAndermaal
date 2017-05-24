243dbf619f7154e566d09b5c76f37024be7a1d09
luukdewildt <dewildtluuk@gmail.com>
2017-05-24 13:34:37 +0200

    Cleanup

--- php/api.php
+++ php/api.php
@@ -302,16 +302,6 @@ function sluitVeiling($data){
     }
 }
 
-//verzenden Email
-function verzendEmail($data){
-    $to = "sinke.carsten95@gmail.com";
-    $subject = "verzendEmail";
-    $txt = "Hello world!";
-    $headers = "From: info@EenmaalAndermaal.nl";
-    mail($to,$subject,$txt,$headers);
-}
-
-
 //registreren van veiling
 function aanmakenveiling($veiling){
     $veiling['verkoperGebruikersnaam'] = "((marion))";
