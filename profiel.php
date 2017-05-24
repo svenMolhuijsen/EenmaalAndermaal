44ee9dbfd6f5be73bbc26ecaa94eb8f38e68f80b
Stein Carsten Sinke <sinke.carsten95@icloud.com>
2017-05-24 16:37:09 +0200

    overzicht page code update

--- profiel.php
+++ profiel.php
@@ -23,85 +23,70 @@ $lopendeBiedingen = executeQuery("SELECT titel, beschrijving, verkoopPrijs from 
 
         <div class="tabs-content" data-tabs-content="profieltabs" data-active-collapse="true">
             <div class="tabs-panel" id="overzicht">
-              <div class = "row">
-                  <h4 ><strong>Algemene accountinstellingen</strong></h4>
-                  <hr>
-                <div class="row columns small-5 float-left">
-                        <h5><strong>Gebruikersnaam en wachtwoord</strong></h5>
-                    <button  id="showEditFormContact"  type="button" class = "button hollow tiny">Edit</button>
-                    <p>Gebruikersnaam: <?php echo($user->getGebruikersnaam());?>
-                        <br>
-                        Wachtwoord:
-                            <?php echo($user->getWachtwoord());?>
-                            <input id = "editWachtwoord" type="text" placeholder = "New Password" style = "width: 150px; height: 25px;  ">
-                    </p>
+                <h4 >Algemene accountinstellingen</h4>
+                <div class = "row small-up-1 medium-up-2">
+                                  
+                    <div class="columns small-6">
+                        <h5>Gebruikersnaam en wachtwoord</h5><hr>
+                        <button  id="showEditFormContact"  type="button" class = "button hollow tiny">Edit</button>
+                        <?php echo('<p>Gebruikersnaam: '.$user->getGebruikersnaam().'</p>'); echo('<p>Wachtwoord:'.$user->getWachtwoord().'<p>');?>
+                        <input id="editWachtwoord" type="password" placeholder="New Password" style="width: 300px; ">
+                    </div>
+
+                    <div class="columns small-6">
+                        <h5>Naam en geboortedatum</h5><hr>
+                        <button  id="showEditFormContact"  type="button" class = "button hollow tiny">Edit</button>
+                        <?php echo('<p>Naam: '.$user->getVoornaam().' '.$user->getAchternaam().'</p>');?>
+                        <input id="editNaam" type="text" placeholder"Voornaam" style="width:300px;">
+                        <?php echo('<p>Geboortedatum: '.$user->getGeboortedatum().'</p>'); ?>
+                        <input id="editGeboortedatum" type="date" style="width:300px;">
+                    </div>
+              </div>
+              <div class="row small-up-1 medium-up-2">
+                <div class="columns small-6">
+                    <h5>Adres</h5><hr>
+                        <button id="showEditFormContact"  type="button" class="button tiny hollow">Edit</button>
+                        <?php echo('<p>Provincie: '.$user->getProvincie().'</p>');
+                        echo('<p>Plaats: '.$user->getPlaatsNaam().'</p>');
+                        echo('<p>Straat :'.$user->getStraatnaam().' '.$user->getHuisnummer().'</p>');
+                        ?>
+                        <input id = "editProvincie" type="text" placeholder = "Provincie" style = "width: 300px;">
+                        <input id = "editPlaats" type="text" placeholder = "Plaats"  style = "width: 300px;">
+                        <input id = "editStraat" type="text" placeholder = "Straat"  style = "width: 300px;">
+                        <input id = "editHuisnummer" type="text" placeholder = "Huisnummer" style = "width: 300px;">
                 </div>
 
-                <div class="row columns small-6 float-right">
-                    <h5><strong>Naam en geboortedatum</strong></h5>
+                <div class="columns small-6">
+                    <h5>Jouw contact gegevens</h5><hr>
                     <button  id="showEditFormContact"  type="button" class = "button hollow tiny">Edit</button>
-                    <br>
-                    Naam:
-                    <?php echo($user->getVoornaam()." ".$user->getAchternaam())?>
-                        <input id = "editNaam" type="text" placeholder = "Nieuwe naam"  style = "width: 150px; height: 25px;  ">
-                        <br>
-                        Geboortedatum:
-                        <?php echo($user->getGeboortedatum()) ?>
-                            <input id = "editGeboortedatum" type="date" placeholder = "Nieuwe gebruikersnaam" style = "width: 200px; height: 40px; ">
-
+                    <?php echo('<p>Telefoonnummer: '.$user->getTelefoonnmr().'</p>'); ?>
+                    <input id = "editTelefoonnummer" type="text" placeholder = "Telefoonnmr" style = "width: 300px;">
                 </div>
               </div>
-              <div class="row">
-                <div class="row  columns small-5 float-left">
-                    <h5><strong>Adres</strong></h5>
-                        <button  id="showEditFormContact"  type="button" class = "button tiny hollow " style = "border-color: #0a0a0a;  background-color: white; color: #0a0a0a">Edit</button>
-                            <br>
-                        Provincie: <?php echo($user->getProvincie())?>
-                        <input id = "editProvincie" type="text" placeholder = "Nieuwe naam" style = "width: 150px; height: 25px;  ">
-                        <br>
-                        Plaats: <?php echo($user->getPlaatsnaam())?>
-                        <input id = "editPlaats" type="text" placeholder = "Nieuwe naam"  style = "width: 150px; height: 25px;  ">
-                        <br>
-                        Straat: <?php echo ($user->getStraatnaam())?>
-                        <input id = "editStraat" type="text" placeholder = "Nieuwe naam"  style = "width: 150px; height: 25px;  ">
-                        <br>
-                        Huisnummer: <?php echo ($user->getHuisnummer())?>
-                        <input id = "editHuisnummer" type="text" placeholder = "Nieuwe naam" style = "width: 150px; height: 25px;  ">
-
-
-                        </p>
-                </div>
-
-                <div class="row columns small-6 float-right">
-                    <h5><strong>Jouw contact gegevens</strong></h5>
-                    <button  id="showEditFormContact"  type="button" class = " small-6 columns button tiny large-1 float-right" style = "border-color: #0a0a0a; margin-top: -35px; background-color: white; color: #0a0a0a">Edit</button>
-                    <p>Telefoon nummer: <?php echo($user->getTelefoonnmr())?>
-                    <input id = "editTelefoonnummer" type="text" placeholder = "Nieuwe naam" style = "width: 150px; height: 25px;  ">
-                    </p>
-                </div>
-              </div>
-               <button type="button" id="submitChanges" class="button large float-center">Submit Changes</button>
-
-
-            <div class="tabs-panel" id="biedingen">
-                <div class="row  columns">
-                    <h4><strong>Mijn biedingen</strong></h4>
-                    <hr>
-                    <h5>Jouw lopende biedingen: </h5>
-                    <p><strong>Titel: </strong><?php echo($lopendeBiedingen['data'][0]['titel']) ?>
-                    <br>
-                       <strong>Omschrijving: </strong> <?php echo($lopendeBiedingen['data'][0]['beschrijving']) ?>
-                    <br>
-                      <strong> Bedrag: </strong> <?php echo($veilingidBiedingen['data'][0]['biedingsBedrag'])  ?>
-                    </p>
-                    <hr>
-                    <h5><strong>Jouw gewonnen biedingen</strong></h5>
-                    <p>Get biedingen</p>
-                    <hr>
-                    <h5><strong>Jouw verlopen biedingen</strong></h5>
-                    <p>Get biedingen</p>
-               </div>
+              <hr>
+              <button type="button" id="submitChanges" class="button large">Submit Changes</button>
             </div>
+               
+
+                <div class="tabs-panel" id="biedingen">
+                    <div class="row  columns">
+                        <h4><strong>Mijn biedingen</strong></h4>
+                        <hr>
+                        <h5>Jouw lopende biedingen: </h5>
+                        <p><strong>Titel: </strong><?php echo($lopendeBiedingen['data'][0]['titel']) ?>
+                        <br>
+                        <strong>Omschrijving: </strong> <?php echo($lopendeBiedingen['data'][0]['beschrijving']) ?>
+                        <br>
+                        <strong> Bedrag: </strong> <?php echo($veilingidBiedingen['data'][0]['biedingsBedrag'])  ?>
+                        </p>
+                        <hr>
+                        <h5><strong>Jouw gewonnen biedingen</strong></h5>
+                        <p>Get biedingen</p>
+                        <hr>
+                        <h5><strong>Jouw verlopen biedingen</strong></h5>
+                        <p>Get biedingen</p>
+                    </div>
+                </div>
             <?php /*?><div class="tabs-panel categoriemanager" id="categorie">
                 <div class="row expanded" style="margin-top:0;">
                     <input type="text" placeholder="new Categorie">
