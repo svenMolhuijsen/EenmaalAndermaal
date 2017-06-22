60965052507ceffd697b28f66d5df87344c9879b
Stein Carsten Sinke <sinke.carsten95@icloud.com>
2017-06-22 11:23:12 +0200

    match code with app.js

--- veilingpagina.php
+++ veilingpagina.php
@@ -186,29 +186,29 @@ $(document).ready(function(){
     var countDownDate = new Date(eindDatum).getTime();
 
     // Update the count down every 1 second
-    var x = setInterval(function() {
+    setInterval(function() {
 
-        // Get todays date and time
-        var now = new Date().getTime();
+    // Get todays date and time
+    var now = new Date().getTime();
 
-        // Find the distance between now an the count down date
-        var distance = countDownDate - now;
+    // Find the distance between now an the count down date
+    var distance = countDownDate - now;
 
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Output the result in an element with id="timer"
-        document.getElementById("timer").innerHTML = days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ";
-
-        // If the count down is over, write some text
-        if (distance < 0) {
-            clearInterval(x);
+    // If the count down is over, write some text
+        if(distance < 0){
+            //clearInterval(x);
             document.getElementById("timer").innerHTML = "VERLOPEN";
             document.getElementById("expired").innerHTML = "";
+        } else {
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Output the result in an element with id="timer"
+            document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+                + minutes + "m " + seconds + "s ";
         }
     }, 1000);
 
